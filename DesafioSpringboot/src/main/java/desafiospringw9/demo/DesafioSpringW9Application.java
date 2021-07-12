995fa40782076e69fc9ba6838b93398300b428ac
Jeremias Santochi <jeremias.santochi@mercadolibre.com>
2021-07-12 16:14:36 -0300

    initial project template

---
+++ DesafioSpringboot/src/main/java/desafiospringw9/demo/DesafioSpringW9Application.java
@@ -0,0 +1,13 @@
+package desafiospringw9.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringW9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSpringW9Application.class, args);
+    }
+
+}
---
+++ DesafioSpringboot/src/test/java/desafiospringw9/demo/DesafioSpringW9ApplicationTests.java
@@ -0,0 +1,13 @@
+package desafiospringw9.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringW9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
