9581fe5d15201640d40b3540e0dc8db2bab475b5
rorellana-meli <ricardo.orellana@mercadolibre.cl>
2021-07-12 23:21:05 -0400

    Add a controller to handle user endpoints

---
+++ SocialMeli/src/main/java/com/mercadolibre/socialmeli/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.mercadolibre.socialmeli.controller;
+
+import com.mercadolibre.socialmeli.dto.response.UserWithFollowedDTO;
+import com.mercadolibre.socialmeli.dto.response.UserWithFollowersCountDTO;
+import com.mercadolibre.socialmeli.dto.response.UserWithFollowersDTO;
+import com.mercadolibre.socialmeli.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@Validated
+@RestController
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/users/{userId}/follow/{userIdToFollow}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String follow(
+            @PathVariable int userId, @PathVariable int userIdToFollow
+    ){
+        userService.followUser(userId, userIdToFollow);
+        return "successfully followed";
+    }
+
+    @GetMapping("/users/{userId}/followers/count/")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public UserWithFollowersCountDTO getUserWithFollowersCount(
+            @PathVariable int userId
+    ){
+        return userService.getUserWithFollowersCountDTO(userId);
+    }
+
+    @GetMapping("/users/{UserID}/followers/list")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public UserWithFollowersDTO getUserWithFollowersDTO(
+            @PathVariable int UserID
+    ){
+        return userService.getUSerWithFollowers(UserID);
+    }
+
+    @GetMapping("/users/{UserID}/followed/list")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public UserWithFollowedDTO getUserWithFollowed(
+            @PathVariable int UserID
+    ){
+        return userService.getUSerWithFollowed(UserID);
+    }
+}
