9bc16fb5c94631cfe317400e47021e88bb716f19
rorellana-meli <ricardo.orellana@mercadolibre.cl>
2021-07-13 14:39:08 -0400

    Add new exceptions to handle errors

--- SocialMeli/src/main/java/com/mercadolibre/socialmeli/exception/GlobalExceptionHandler.java
+++ SocialMeli/src/main/java/com/mercadolibre/socialmeli/exception/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.mercadolibre.socialmeli.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.format.DateTimeParseException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
@@ -25,6 +31,30 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorMessage handlerUserAlreadyFollowSeller(UserAlreadyFollowSellerException ex){
         return  new ErrorMessage(ex.getStatus().value(), ex.getReason());
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorMessage handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        StringBuilder errorMessage = new StringBuilder("");
+        errors.forEach((__, message) -> errorMessage.append(message));
+        return new ErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY.value(), errorMessage.toString());
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+    @ExceptionHandler(DateTimeParseException.class)
+    public ErrorMessage handleValidationDateExceptions(
+            DateTimeParseException ex) {
+        return new ErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY.value(), ex.getMessage());
     }
 }
 
