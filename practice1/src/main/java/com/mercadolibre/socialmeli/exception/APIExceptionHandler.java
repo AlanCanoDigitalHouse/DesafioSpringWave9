7e63a04be1c7d2bd35cbb72e7468026031e1a23f
Luis Moreno <luis.moreno@mercadolibre.com.co>
2021-07-14 09:22:59 -0500

    API simple exception handler.

---
+++ practice1/src/main/java/com/mercadolibre/socialmeli/exception/APIExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.mercadolibre.socialmeli.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class APIExceptionHandler {
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handlerException(MethodArgumentNotValidException exception) {
+        BindingResult result = exception.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        return fieldErrors.get(0).getDefaultMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handlerException(ServiceException exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public void handlerException(Exception exception) {
+    }
+}
