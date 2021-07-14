c7c5f8ead35c59de1a0c24bd23453e50940858b7
AriSchermuk <ariel.schermuk@mercadolibre.com>
2021-07-14 11:35:28 -0300

    Simplified Exception Handling

--- Desafio-Spring/src/main/java/com/example/desafiospring/exceptions/APIExceptionHandler.java
+++ Desafio-Spring/src/main/java/com/example/desafiospring/exceptions/APIExceptionHandler.java
@@ -1,12 +1,7 @@
 package com.example.desafiospring.exceptions;
 
-import com.example.desafiospring.exceptions.general.DBNotAvailableException;
-import com.example.desafiospring.exceptions.general.NoSuchElementInDBException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class APIExceptionHandler {
@@ -14,46 +9,9 @@ public class APIExceptionHandler {
     @ExceptionHandler
     @ResponseBody
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleException(NoSuchElementInDBException exception) {
-        return handleGeneralException(exception);
-    }
-
-    @ExceptionHandler
-    @ResponseBody
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleException(DBNotAvailableException exception) {
-        return handleGeneralException(exception);
-    }
-
-    @ExceptionHandler
-    @ResponseBody
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorMessage handleGeneralException(Exception exception){
+    public ErrorMessage handleGeneralException(Exception exception) {
         exception.printStackTrace();
         String message = exception.getMessage();
-        Map<String,String> details = getDetails (exception);
-        return new ErrorMessage(message, details);
+        return new ErrorMessage(message);
     }
-
-    private Map<String, String> getDetails(Throwable exception) {
-        Map<String, String> details = new HashMap<>();
-        Throwable cause = exception.getCause();
-        if (cause!=null &&cause!=exception) {
-            details.put("CAUSE", cause.toString());
-            details.put("STACK_TRACE", stackTraceToString(cause.getStackTrace()));
-            details.putAll(getDetails(cause));
-        }
-        return details;
-    }
-
-    private String stackTraceToString(StackTraceElement[] stackTrace) {
-        StringBuilder stackTraceAsString= new StringBuilder();
-        for (StackTraceElement element:
-             stackTrace) {
-            stackTraceAsString.append(element.toString()).append(System.lineSeparator());
-        }
-        return stackTraceAsString.toString();
-    }
-
-
 }
