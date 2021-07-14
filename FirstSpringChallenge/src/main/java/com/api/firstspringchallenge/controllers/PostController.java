f7363bbd183462ebf99b57cf9bbdd5a7e0ff6fd0
Karen Manrique <karen.manrique@mercadolibre.com>
2021-07-14 16:29:17 -0300

    :sparkles: Adding user and post controllers

---
+++ FirstSpringChallenge/src/main/java/com/api/firstspringchallenge/controllers/PostController.java
@@ -0,0 +1,43 @@
+package com.api.firstspringchallenge.controllers;
+
+import com.api.firstspringchallenge.dtos.request.PostRequestDTO;
+import com.api.firstspringchallenge.dtos.request.PostsRequestDTO;
+import com.api.firstspringchallenge.dtos.request.PromoPostRequestDTO;
+import com.api.firstspringchallenge.services.post.implementation.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@AllArgsConstructor
+@RestController
+public class PostController {
+    private final PostService service;
+
+    @PostMapping("/products/newpost")
+    public ResponseEntity<Void> newPost(@Valid @RequestBody PostRequestDTO post) {
+        return service.newPost(post);
+    }
+
+    @GetMapping("/products/followed/{userId}/list")
+    public ResponseEntity<Void> getPostsBy(@Valid PostsRequestDTO request) {
+        return service.getPostsBy(request.getUserId(), request.getOrder());
+    }
+
+    @PostMapping("/products/newpromopost")
+    public ResponseEntity<Void> newPromoPost(@Valid @RequestBody PromoPostRequestDTO request) {
+        return service.newPromoPost(request);
+    }
+
+    @GetMapping("/products/{userId}/countPromo")
+    public ResponseEntity<Void> promoQuantity(@PathVariable("userId") int userId) {
+        return service.getPromoQuantity(userId);
+    }
+
+    @GetMapping("/products/{userId}/list")
+    public ResponseEntity<Void> promoList(@PathVariable("userId") int userId) {
+        return service.getPromoList(userId);
+    }
+
+}
---
+++ FirstSpringChallenge/src/main/java/com/api/firstspringchallenge/controllers/UsersController.java
@@ -0,0 +1,49 @@
+package com.api.firstspringchallenge.controllers;
+
+import com.api.firstspringchallenge.dtos.request.FollowersListRequestDTO;
+import com.api.firstspringchallenge.dtos.response.FollowedResponseDTO;
+import com.api.firstspringchallenge.dtos.response.FollowersCountResponseDTO;
+import com.api.firstspringchallenge.dtos.response.FollowersResponseDTO;
+import com.api.firstspringchallenge.services.user.implementation.SellerService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@AllArgsConstructor
+public class UsersController {
+
+    private final SellerService service;
+
+    @PostMapping("/users/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<Void> followSeller(@PathVariable("userId") int userId, @PathVariable("userIdToFollow") int userIdToFollow) {
+        return service.follow(userId, userIdToFollow);
+    }
+
+    @GetMapping("/users/{userId}/followers/count")
+    public ResponseEntity<FollowersCountResponseDTO> followersQuantity(@PathVariable("userId") int userId) {
+        return service.getFollowersQuantity(userId);
+    }
+
+    @GetMapping("/users/{userId}/followers/list")
+    public ResponseEntity<FollowersResponseDTO> followers(@Valid FollowersListRequestDTO request) {
+        return service.getFollowers(request.getUserId(), request.getOrder());
+    }
+
+    @GetMapping("/users/{userId}/followed/list")
+    public ResponseEntity<FollowedResponseDTO> followed(@PathVariable("userId") int userId) {
+        return service.getFollowed(userId);
+    }
+
+    @PostMapping("/users/{userId}/unfollow/{userIdToFollow}")
+    public ResponseEntity<Void> unfollowSeller(@PathVariable("userId") int userId, @PathVariable("userIdToFollow") int userIdToFollow) {
+        return service.unfollow(userId, userIdToFollow);
+    }
+
+
+}
