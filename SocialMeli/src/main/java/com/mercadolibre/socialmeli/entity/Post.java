1044d01566ae027367f8a7db635e48ee53872c0d
rorellana-meli <ricardo.orellana@mercadolibre.cl>
2021-07-13 22:00:31 -0400

    Add annotations to change variable name when object is serialized and change data format

--- SocialMeli/src/main/java/com/mercadolibre/socialmeli/entity/Post.java
+++ SocialMeli/src/main/java/com/mercadolibre/socialmeli/entity/Post.java
@@ -1,18 +1,31 @@
 package com.mercadolibre.socialmeli.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    int userId;
     int id_post;
-    Date date;
-    Product product;
+    LocalDate createdDate;
+    @NotNull(message = "detail cannot be null")
+    Product detail;
     int category;
     double price;
+
+    @JsonProperty("date")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
 }
