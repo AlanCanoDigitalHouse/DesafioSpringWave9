09e848aac04db7a6875d61ff6381833280dacca2
Karen Manrique <karen.manrique@mercadolibre.com>
2021-07-15 10:44:24 -0300

    :wrench: Fixing category enum

--- FirstSpringChallenge/src/main/java/com/api/firstspringchallenge/enumerates/Category.java
+++ FirstSpringChallenge/src/main/java/com/api/firstspringchallenge/enumerates/Category.java
@@ -1,5 +1,19 @@
 package com.api.firstspringchallenge.enumerates;
 
 public enum Category {
-    SILLAS, SOFAS, TECNOLOGÍA
+    SILLAS(0), TECLADOS(1);
+
+    private int value;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    private Category(int value){
+        this.value=value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
