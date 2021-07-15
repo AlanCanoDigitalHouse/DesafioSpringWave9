6df03670dbc5f65b042b177e6d7ddf9a7f7b42a3
Alonso Chareun <alonso.chareun@mercadolibre.com>
2021-07-15 08:37:49 -0300

    add missing validation constraints on product

--- SpringChallenge/src/main/java/com/mercadolibre/social_meli/entity/Product.java
+++ SpringChallenge/src/main/java/com/mercadolibre/social_meli/entity/Product.java
@@ -2,6 +2,9 @@ package com.mercadolibre.social_meli.entity;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,10 +12,25 @@ import lombok.*;
 public class Product {
 
     private Integer product_id;
+
+    @NotNull(message = "A product name is needed")
+    @NotBlank(message = "A product name is needed")
     private String productName;
+
+    @NotNull(message = "A product type is needed")
+    @NotBlank(message = "A product type is needed")
     private String type;
+
+    @NotNull(message = "A product brand is needed")
+    @NotBlank(message = "A product brand is needed")
     private String brand;
+
+    @NotNull(message = "A product brand is needed")
+    @NotBlank(message = "A product brand is needed")
     private String color;
+
+    @NotNull(message = "A product brand is needed")
+    @NotBlank(message = "A product brand is needed")
     private String notes;
 
 }
