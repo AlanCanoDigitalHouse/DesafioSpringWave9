924f66817c9835c03a86b15759919d931cf5fa8d
Alonso Chareun <alonso.chareun@mercadolibre.com>
2021-07-22 17:05:39 -0300

    add missing dtos annotations

--- 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/dto/DistrictDTO.java
+++ 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/dto/DistrictDTO.java
@@ -1,7 +1,15 @@
 package com.mercadolibre.tucasitatasaciones.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DistrictDTO {
 
     @JsonProperty(value = "district_name")
--- 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/dto/request/PropertyRequestDTO.java
+++ 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/dto/request/PropertyRequestDTO.java
@@ -3,10 +3,18 @@ package com.mercadolibre.tucasitatasaciones.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mercadolibre.tucasitatasaciones.dto.DistrictDTO;
 import com.mercadolibre.tucasitatasaciones.dto.EnvironmentDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropertyRequestDTO {
 
     @JsonProperty(value = "prop_name")
