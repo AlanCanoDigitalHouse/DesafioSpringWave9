225f0d4e255c16bf358278c776912b6d63a37b89
Your Name <esteban.guruceaga@mercadolibre.com>
2021-07-22 16:49:09 -0300

    Creacion de proyecto.

---
+++ 9_desafio2/tucasita/src/main/java/com/desafio2/spring/tucasita/tucasita/TucasitaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio2.spring.tucasita.tucasita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucasitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TucasitaApplication.class, args);
+    }
+
+}
---
+++ 9_desafio2/tucasita/src/test/java/com/desafio2/spring/tucasita/tucasita/TucasitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio2.spring.tucasita.tucasita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TucasitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
