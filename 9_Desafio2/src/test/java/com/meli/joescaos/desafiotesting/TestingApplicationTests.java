1eba2ed036d67587169485598a699cc2527e3edb
johanCanas <johan.canas@mercadolibre.com.co>
2021-07-22 14:24:57 -0500

    project setup

---
+++ 9_Desafio2/src/test/java/com/meli/joescaos/desafiotesting/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.joescaos.desafiotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
