6222d26582cd43e3394835ffcf8c7d51992afeca
Alonso Chareun <alonso.chareun@mercadolibre.com>
2021-07-22 15:44:29 -0300

    init spring boot project

---
+++ 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/TucasitaTasacionesApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.tucasitatasaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucasitaTasacionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TucasitaTasacionesApplication.class, args);
+    }
+
+}
