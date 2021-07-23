9a5abd6020c9d8eda98cc092556903d508da6896
Alonso Chareun <alonso.chareun@mercadolibre.com>
2021-07-23 16:16:35 -0300

    add error handling methods to controller advice

--- 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/controller/APIExceptionController.java
+++ 9_desafio2/src/main/java/com/mercadolibre/tucasitatasaciones/controller/APIExceptionController.java
@@ -1,7 +1,13 @@
 package com.mercadolibre.tucasitatasaciones.controller;
 
+import com.mercadolibre.tucasitatasaciones.dto.response.ErrorDTO;
 import com.mercadolibre.tucasitatasaciones.dto.response.ErrorWithFieldsDTO;
+import com.mercadolibre.tucasitatasaciones.exception.DatabaseException;
+import com.mercadolibre.tucasitatasaciones.exception.InvalidRequestData;
+import com.mercadolibre.tucasitatasaciones.exception.ResourceNotFoundException;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +18,45 @@ import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class APIExceptionController {
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleRequestError(ResourceNotFoundException exception) {
+        return new ErrorDTO(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleRequestError(InvalidRequestData exception) {
+        return new ErrorDTO(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleRequestError(HttpMessageNotReadableException exception) {
+        var message = "An error occurred while parsing your HTTP request";
+
+        return new ErrorDTO(HttpStatus.BAD_REQUEST.value(), message);
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleInternalError(DatabaseException exception) {
+        return new ErrorDTO(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleInternalError(JsonParseException exception) {
+        var message = "An error occurred while parsing the JSON";
+
+        return new ErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), message);
+    }
 
     @ExceptionHandler
     @ResponseBody
