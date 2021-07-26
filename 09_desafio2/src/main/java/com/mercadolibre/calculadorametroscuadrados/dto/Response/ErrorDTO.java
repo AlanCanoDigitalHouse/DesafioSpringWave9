7d28568cd25006f337b715173310e9dababea92c
Jose Andres Lopez <jose.lopez@mercadolibre.com.co>
2021-07-26 15:37:12 -0500

    Update Readme Requirements

--- 09_desafio2/src/main/java/com/mercadolibre/calculadorametroscuadrados/dto/Response/ErrorDTO.java
+++ 09_desafio2/src/main/java/com/mercadolibre/calculadorametroscuadrados/dto/Response/ErrorDTO.java
@@ -3,13 +3,9 @@ package com.mercadolibre.calculadorametroscuadrados.dto.Response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ErrorDTO {
     private String name;
     private String description;
