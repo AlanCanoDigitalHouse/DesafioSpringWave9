a1efefb198cc8eded1208df94f0779a4e420ca1c
Jeremias Santochi <jeremias.santochi@mercadolibre.com>
2021-07-26 14:41:03 -0300

    Exception

---
+++ 9_desafio2/src/main/java/testingchallengue/demo/exceptions/ExistentDistrictNameException.java
@@ -0,0 +1,10 @@
+package testingchallengue.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ExistentDistrictNameException extends EstateException {
+
+    public ExistentDistrictNameException(String districtName) {
+        super("District name' " + districtName + " '  is already in use ", HttpStatus.BAD_REQUEST);
+    }
+}
