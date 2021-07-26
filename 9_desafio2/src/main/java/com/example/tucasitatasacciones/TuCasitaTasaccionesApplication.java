cad48af10b73e300d9df5d8133712afdd4a9a36c
JSebastianMG <juan.mancera@mercadolibre.com.co>
2021-07-26 00:19:35 -0500

    Spring Project created and folders

---
+++ 9_desafio2/src/main/java/com/example/tucasitatasacciones/TuCasitaTasaccionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tucasitatasacciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuCasitaTasaccionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuCasitaTasaccionesApplication.class, args);
+    }
+
+}
