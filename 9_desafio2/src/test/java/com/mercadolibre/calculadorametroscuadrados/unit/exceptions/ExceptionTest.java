b3b5b99b0bf414f99cc0cef85ee93d2eb729a29a
acarretero <andres.carretero@mercadolibre.com>
2021-07-26 17:58:12 -0300

    Added Validation Testing

--- 9_desafio2/src/test/java/com/mercadolibre/calculadorametroscuadrados/unit/exceptions/ExceptionTest.java
+++ 9_desafio2/src/test/java/com/mercadolibre/calculadorametroscuadrados/unit/exceptions/ExceptionTest.java
@@ -1,23 +1,40 @@
 package com.mercadolibre.calculadorametroscuadrados.unit.exceptions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mercadolibre.calculadorametroscuadrados.dto.LocationDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.requests.HouseRequestDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.response.HouseResponseDTO;
 import com.mercadolibre.calculadorametroscuadrados.dto.response.ResponseErrorDto;
 import com.mercadolibre.calculadorametroscuadrados.exceptions.ApiExceptionsControllerAdvice;
+import com.mercadolibre.calculadorametroscuadrados.exceptions.ErrorMessage;
 import com.mercadolibre.calculadorametroscuadrados.exceptions.IncorrectLocationPrice;
 import com.mercadolibre.calculadorametroscuadrados.exceptions.LocationNotFound;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @SpringBootTest
 public class ExceptionTest {
 
 
+    @InjectMocks
+    private ApiExceptionsControllerAdvice apiExceptionsControllerAdvice;
+
     @Test
     void locationNotFoundExceptionTest(){
-        ApiExceptionsControllerAdvice apiExceptionsControllerAdvice = new ApiExceptionsControllerAdvice();
 
         LocationNotFound locationNotFound = new LocationNotFound();
         ResponseErrorDto expectedErrorMessage =
@@ -33,8 +50,6 @@ public class ExceptionTest {
 
     @Test
     void incorrectLocationPriceExceptionTest(){
-        ApiExceptionsControllerAdvice apiExceptionsControllerAdvice = new ApiExceptionsControllerAdvice();
-
         IncorrectLocationPrice locationNotFound = new IncorrectLocationPrice();
         ResponseErrorDto expectedErrorMessage =
                 new ResponseErrorDto(HttpStatus.NOT_FOUND.value(),
@@ -45,5 +60,28 @@ public class ExceptionTest {
         ResponseEntity<ResponseErrorDto> errorMessage = apiExceptionsControllerAdvice.incorrectLocationPriceExceptionHandler(locationNotFound);
 
         Assertions.assertEquals(expectedErrorResponse,errorMessage);
+    }
+
+    @Test
+    void multipleValidationErrorTest() throws JsonProcessingException {
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+        BeanPropertyBindingResult beanPropertyBindingResult = new BeanPropertyBindingResult(HouseRequestDTO.class,"Error");
+        beanPropertyBindingResult.addError(new FieldError("HouseRequestDto","name","El nombre de la propiedad no puede estar vacío."));
+        beanPropertyBindingResult.addError(new FieldError("HouseRequestDto","rooms[0].length","El máximo largo permitido por propiedad es de 33 mts"));
+
+        MethodArgumentNotValidException methodArgumentNotValidException = new MethodArgumentNotValidException(null,beanPropertyBindingResult);
+
+        Map<String, String> message = new HashMap<>();
+        message.put("name","El nombre de la propiedad no puede estar vacío.");
+        message.put("rooms[0].length","El máximo largo permitido por propiedad es de 33 mts");
+        ErrorMessage expectedMessage = new ErrorMessage(400,"Validations Error", message);
+
+        ErrorMessage errorMessage = apiExceptionsControllerAdvice.handlerException(methodArgumentNotValidException);
+
+        Assertions.assertEquals(writer.writeValueAsString(expectedMessage), writer.writeValueAsString(errorMessage));
     }
 }
