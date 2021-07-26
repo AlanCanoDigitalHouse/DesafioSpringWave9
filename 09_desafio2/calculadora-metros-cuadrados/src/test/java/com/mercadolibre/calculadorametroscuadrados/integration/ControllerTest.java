c9373b45c92b313217ed11f12775cdaa8c805bc1
Cristobal Alberto Zepeda Tello <czepeda@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-07-26 09:51:07 -0400

    testIntegrales agregado

--- 09_desafio2/calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/integration/ControllerTest.java
+++ 09_desafio2/calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/integration/ControllerTest.java
@@ -1,10 +1,19 @@
 package com.mercadolibre.calculadorametroscuadrados.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -14,16 +23,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class ControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    public void testPathVarEndPoint() throws Exception{
-//        this.mockMvc.perform(get("/{location}","Palermo"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType("application/json"))
-//                .andExpect(jsonPath("$.price").value(1000))
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testPathVarEndPoint() throws Exception{
+        this.mockMvc.perform(get("/{location}","Palermo"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.price").value(1000))
+                .andReturn();
+    }
+
+    @Test
+    public void testPathVarEndPoint2() throws Exception{
+        this.mockMvc.perform(get("/{location}","Recoleta"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.price").value(900))
+                .andReturn();
+    }
+
+    @Test
+    public void testBadPost() throws Exception{
+        RoomDTO room1 = new RoomDTO("Pieza", 10000,1);
+        RoomDTO room2 = new RoomDTO("Pieza2", 10,1);
+        List<RoomDTO> Lista = new ArrayList<>();
+        Lista.add(room1);
+        Lista.add(room2);
+        HouseDTO casa1 = new HouseDTO("Casa","Prueba","Palermo", Lista,1000);
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
+
+        String payloadJSON = writer.writeValueAsString(casa1);
+
+        this.mockMvc.perform(post("/calculate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payloadJSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType("application/json"));
+    }
 
 }
