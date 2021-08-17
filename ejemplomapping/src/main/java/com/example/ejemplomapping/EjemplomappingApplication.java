293925747bee40bdb180d1f2803e3ba8df075205
Miguel Mestra <miguel.mestra@mercadolibre.com.co>
2021-08-17 10:54:52 -0500

    Prueba git

---
+++ ejemplomapping/src/main/java/com/example/ejemplomapping/EjemplomappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemplomapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplomappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemplomappingApplication.class, args);
+    }
+
+}
---
+++ ejemplomapping/src/test/java/com/example/ejemplomapping/EjemplomappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplomapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplomappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
